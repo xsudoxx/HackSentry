04b7dab809ec54f4c0d5c68751c7a671f757f1e8
Joshua Franco <francoswim@yahoo.com>
2023-11-02 20:35:34 -0400

    Adding arguements -u and -w

--- HackSentry.py
+++ HackSentry.py
@@ -1,1 +1,23 @@
+import argparse
+
+def print_sentry_gun():
+    print("   __,_____")
+    print("  / __.==--'")
+    print(" /#(-'")
+    print("/#_#_#_#_#_#")
+    print("#_#_#_#_#_#_#")
+    print("'-'-'-'-'-'-'")
+
+def main():
+    print_sentry_gun()
+    
+    parser = argparse.ArgumentParser(description="HackSentry flag options below")
+    parser.add_argument('-u','--url',action='store_true',help="Single URL")
+    parser.add_argument('-w','--wordlist',action='store_true',help='Wordlists to iterate through')
+    parser.parse_args()
+
+
+
+if __name__ == "__main__":
+    main()
 
