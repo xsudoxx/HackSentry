c873991fbe96fa6de30f6ec4a70b9f27b81afdab
Joshua Franco <francoswim@yahoo.com>
2023-11-02 21:19:06 -0400

    removing init.py files

--- tests/__init__.py
+++
@@ -1,2 +0,0 @@
-# This file is used to mark a directory as a Python package. If you have a directory that you want to treat as a package, you should include a '__init__.py' file in that directory.
-# This file can be empty or contain initialization code that you want to run when the package is imported.
