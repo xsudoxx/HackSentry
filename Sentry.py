2a6f42f506ae77d54ef3e1a0cb628b414ede5345
Joshua Franco <francoswim@yahoo.com>
2023-11-08 15:49:53 -0500

    This modification allows the regular expression to match domain names with a length of up to 63 characters, accommodating various top-level domains like .io.

--- Sentry.py
+++ Sentry.py
@@ -15,12 +15,13 @@ def print_sentry_gun():
 def check_url(url, port=None, wordlist=None, output=None):
     regex = re.compile(
         r'^(?:http|ftp)s?://'  # http:// or https://
-        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,63}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
         r'localhost|'  # localhost...
         r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
         r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
         r'(?::\d+)?'  # optional port
         r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
 
     if re.match(regex, url):
         if wordlist and port and output:
